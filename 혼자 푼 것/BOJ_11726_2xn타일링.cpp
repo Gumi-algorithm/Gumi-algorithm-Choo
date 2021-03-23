57e2b6bbf2bd4f41255c1160f2ef6172aea3589e
추헌국 <cngjsrnr@naver.com>
2021-03-23 14:02:59 +0900

    BOJ_11726_2xn타일링(DP)

--- 혼자 푼 것/BOJ_11726_2xn타일링.cpp
+++ 혼자 푼 것/BOJ_11726_2xn타일링.cpp
@@ -6,17 +6,30 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int n;
+	cin >> n;
+	/*
 	int dp[1001];
 	dp[0] = 0;
 	dp[1] = 1;
 	dp[2] = 2;
-	int n;
-	cin >> n;
+
 
 	for (int i = 3; i <= n; i++) {
 		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
 	}
 	cout << dp[n];
-
+	*/
+	
+	int a = 1;
+	int b = 1;
+	int tmp;
+	for (int i = 0; i < n - 1; i++) {
+		tmp = (a + b)% 10007;
+		a = b;
+		b = tmp;
+	}
+	cout << b;
+	
 	return 0;
 }
