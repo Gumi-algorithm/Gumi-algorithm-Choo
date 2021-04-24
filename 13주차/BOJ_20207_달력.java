e198be10bc1a4cdfc5f5c0e5b1f7a7f80640cb02
추헌국 <cngjsrnr@naver.com>
2021-04-24 20:41:54 +0900

    13주차-3 구현 BOJ_20207_달력

--- 13주차/BOJ_20207_달력.java
+++ 13주차/BOJ_20207_달력.java
@@ -3,8 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-public class BOJ_20207_달력 {
-	
+public class BOJ_20207_달력 {	
 	public static void main(String[] args)throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter pw = new PrintWriter(System.out);
